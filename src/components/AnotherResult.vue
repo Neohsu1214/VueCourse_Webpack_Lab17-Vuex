11885573331bf69ddd2f83c16a72044a651e207f
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-12 16:56:11 +0800

    透過實作 getters 來取得『需要處理過』的資料，可以避免重複程式碼的產生，增加維護簡易度

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -7,7 +7,7 @@
         // 要呈現資料的地方，記得要改用 computed 來接收即時更新的 store 資料內容
         computed: {
             counter: function() {
-                return this.$store.state.counter * 2;
+                return this.$store.getters.doubleCounter;
             }
         }
     }
--- src/store/store.js
+++ src/store/store.js
@@ -6,5 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    getters: {
+        doubleCounter: function(state) {  // 也可以簡寫成 doubleCounter: (state) => { return state.counter * 2; }
+            return state.counter * 2;
+        }
     }
 });
