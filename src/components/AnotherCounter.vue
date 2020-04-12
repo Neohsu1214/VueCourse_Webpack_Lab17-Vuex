6cb36a25fbccb6f3e1d0444883874c8f84b951ba
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-12 21:22:55 +0800

    異動 store 中資料的方法 this.$store.commit('mutation函式名稱')

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+                this.$store.commit('increment'); // 異動 store 中資料的方法 this.$store.commit('mutation函式名稱')
+            },
+            decrement() {
+                this.$store.commit('decrement');
+            }
+        }
+    }
+</script>
--- src/store/store.js
+++ src/store/store.js
@@ -7,12 +7,22 @@ export const store = new Vuex.Store({
     state: {
         counter: 0
     },
+    // getters 只能用來 讀取
     getters: {
         doubleCounter: function(state) {  // 也可以簡寫成 doubleCounter: (state) => { return state.counter * 2; }
             return state.counter * 2;
         },
         stringCounter: (state) => {
             return state.counter + ' Clicks';
+        }
+    },
+    // mutations 只能用來 寫值
+    mutations: {
+        increment: (state) => { // 也可以寫成 increment: function(state) { state.counter++; }
+            state.counter++;
+        },
+        decrement: (state) => {
+            state.counter--;
         }
     }
 });
